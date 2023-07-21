3f07d5384610196fea52767c8afc9483989c015c
Robson <robson.v8@hotmail.com>
2023-07-21 19:25:57 -0300

    including a description space that can be edit by user bellow the character in collectionScreen.

--- app/build/tmp/kapt3/stubs/debug/com/catalin/comicslibrary/viewmodel/CollectionDbViewModel.java
+++ app/build/tmp/kapt3/stubs/debug/com/catalin/comicslibrary/viewmodel/CollectionDbViewModel.java
@@ -3,13 +3,15 @@ package com.catalin.comicslibrary.viewmodel;
 import java.lang.System;
 
 @dagger.hilt.android.lifecycle.HiltViewModel
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00008\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0002\b\u0007\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\r\u001a\u00020\u000e2\u0006\u0010\u000f\u001a\u00020\u0010J\u000e\u0010\u0011\u001a\u00020\u000e2\u0006\u0010\u000f\u001a\u00020\bJ\b\u0010\t\u001a\u00020\u000eH\u0002J\u0015\u0010\u0012\u001a\u00020\u000e2\b\u0010\u0013\u001a\u0004\u0018\u00010\u0014\u00a2\u0006\u0002\u0010\u0015R\u001d\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u0019\u0010\u000b\u001a\n\u0012\u0006\u0012\u0004\u0018\u00010\b0\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\nR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0016"}, d2 = {"Lcom/catalin/comicslibrary/viewmodel/CollectionDbViewModel;", "Landroidx/lifecycle/ViewModel;", "repo", "Lcom/catalin/comicslibrary/model/db/CollectionDbRepo;", "(Lcom/catalin/comicslibrary/model/db/CollectionDbRepo;)V", "collection", "Lkotlinx/coroutines/flow/MutableStateFlow;", "", "Lcom/catalin/comicslibrary/model/db/DbCharacter;", "getCollection", "()Lkotlinx/coroutines/flow/MutableStateFlow;", "currentCharacter", "getCurrentCharacter", "addCharacter", "", "character", "Lcom/catalin/comicslibrary/model/CharacterResult;", "deleteCharacter", "setCurrentCharacterId", "characterId", "", "(Ljava/lang/Integer;)V", "app_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000H\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b\u0002\b\u0007\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\u0010\u001a\u00020\u00112\u0006\u0010\u0012\u001a\u00020\u0013J\u000e\u0010\u0014\u001a\u00020\u00112\u0006\u0010\u0015\u001a\u00020\u0016J\u000e\u0010\u0017\u001a\u00020\u00112\u0006\u0010\u0012\u001a\u00020\bJ\u000e\u0010\u0018\u001a\u00020\u00112\u0006\u0010\u0015\u001a\u00020\u000eJ\b\u0010\t\u001a\u00020\u0011H\u0002J\b\u0010\u000f\u001a\u00020\u0011H\u0002J\u0015\u0010\u0019\u001a\u00020\u00112\b\u0010\u001a\u001a\u0004\u0018\u00010\u001b\u00a2\u0006\u0002\u0010\u001cR\u001d\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u0019\u0010\u000b\u001a\n\u0012\u0006\u0012\u0004\u0018\u00010\b0\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\nR\u001d\u0010\r\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u000e0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\nR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001d"}, d2 = {"Lcom/catalin/comicslibrary/viewmodel/CollectionDbViewModel;", "Landroidx/lifecycle/ViewModel;", "repo", "Lcom/catalin/comicslibrary/model/db/CollectionDbRepo;", "(Lcom/catalin/comicslibrary/model/db/CollectionDbRepo;)V", "collection", "Lkotlinx/coroutines/flow/MutableStateFlow;", "", "Lcom/catalin/comicslibrary/model/db/DbCharacter;", "getCollection", "()Lkotlinx/coroutines/flow/MutableStateFlow;", "currentCharacter", "getCurrentCharacter", "notes", "Lcom/catalin/comicslibrary/model/db/DbNote;", "getNotes", "addCharacter", "", "character", "Lcom/catalin/comicslibrary/model/CharacterResult;", "addNote", "note", "Lcom/catalin/comicslibrary/model/Note;", "deleteCharacter", "deleteNote", "setCurrentCharacterId", "characterId", "", "(Ljava/lang/Integer;)V", "app_debug"})
 public final class CollectionDbViewModel extends androidx.lifecycle.ViewModel {
     private final com.catalin.comicslibrary.model.db.CollectionDbRepo repo = null;
     @org.jetbrains.annotations.NotNull
     private final kotlinx.coroutines.flow.MutableStateFlow<com.catalin.comicslibrary.model.db.DbCharacter> currentCharacter = null;
     @org.jetbrains.annotations.NotNull
     private final kotlinx.coroutines.flow.MutableStateFlow<java.util.List<com.catalin.comicslibrary.model.db.DbCharacter>> collection = null;
+    @org.jetbrains.annotations.NotNull
+    private final kotlinx.coroutines.flow.MutableStateFlow<java.util.List<com.catalin.comicslibrary.model.db.DbNote>> notes = null;
     
     @javax.inject.Inject
     public CollectionDbViewModel(@org.jetbrains.annotations.NotNull
@@ -25,6 +27,22 @@ public final class CollectionDbViewModel extends androidx.lifecycle.ViewModel {
     @org.jetbrains.annotations.NotNull
     public final kotlinx.coroutines.flow.MutableStateFlow<java.util.List<com.catalin.comicslibrary.model.db.DbCharacter>> getCollection() {
         return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final kotlinx.coroutines.flow.MutableStateFlow<java.util.List<com.catalin.comicslibrary.model.db.DbNote>> getNotes() {
+        return null;
+    }
+    
+    private final void getNotes() {
+    }
+    
+    public final void addNote(@org.jetbrains.annotations.NotNull
+    com.catalin.comicslibrary.model.Note note) {
+    }
+    
+    public final void deleteNote(@org.jetbrains.annotations.NotNull
+    com.catalin.comicslibrary.model.db.DbNote note) {
     }
     
     private final void getCollection() {
--- app/src/main/java/com/catalin/comicslibrary/viewmodel/CollectionDbViewModel.kt
+++ app/src/main/java/com/catalin/comicslibrary/viewmodel/CollectionDbViewModel.kt
@@ -3,22 +3,48 @@ package com.catalin.comicslibrary.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.catalin.comicslibrary.model.CharacterResult
+import com.catalin.comicslibrary.model.Note
 import com.catalin.comicslibrary.model.db.CollectionDbRepo
 import com.catalin.comicslibrary.model.db.DbCharacter
+import com.catalin.comicslibrary.model.db.DbNote
+import com.catalin.comicslibrary.model.db.NoteDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CollectionDbViewModel @Inject constructor(private val repo: CollectionDbRepo): ViewModel(){
+class CollectionDbViewModel @Inject constructor(private val repo: CollectionDbRepo): ViewModel() {
 
     val currentCharacter = MutableStateFlow<DbCharacter?>(null)
     val collection = MutableStateFlow<List<DbCharacter>>(listOf())
+    val notes = MutableStateFlow<List<DbNote>>(listOf())
 
     init {
         getCollection()
+        getNotes()
+    }
+
+    private fun getNotes() {
+        viewModelScope.launch {
+            repo.getAllNotes().collect{
+                notes.value = it
+            }
+        }
+    }
+
+    fun addNote(note: Note){
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.addNotesToRepo(DbNote.fromNote(note))
+        }
+    }
+
+    fun deleteNote(note: DbNote){
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.deleteNoteFromRepo(note)
+        }
     }
 
     private fun getCollection(){
@@ -47,6 +73,7 @@ class CollectionDbViewModel @Inject constructor(private val repo: CollectionDbRe
 
     fun deleteCharacter(character: DbCharacter){
         viewModelScope.launch(Dispatchers.IO) {
+            repo.deleteAllNotes(character)
             repo.deleteCharacterFromRepo(character)
         }
     }
