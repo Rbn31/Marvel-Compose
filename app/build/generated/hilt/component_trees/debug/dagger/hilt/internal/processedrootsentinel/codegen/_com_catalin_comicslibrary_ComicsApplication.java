236922c799590ffa82471bd425e8ad7a1f5364cc
Robson <robson.v8@hotmail.com>
2023-07-23 21:10:49 -0300

    some fix

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_catalin_comicslibrary_ComicsApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.catalin.comicslibrary.ComicsApplication"
-)
-public final class _com_catalin_comicslibrary_ComicsApplication {
-}
